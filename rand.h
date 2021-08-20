d8e79646d3008df4f44f6dc4b6602195942dedb7
Marshal Horn <kamocat@gmail.com>
2021-08-20 07:26:55 -0700

    Create rand.h

---
+++ rand.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+struct xorshift32_state {
+  uint32_t a;
+};
+
+/* The state word must be initialized to non-zero */
+uint32_t xorshift32(struct xorshift32_state *state)
+{
+	/* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
+	uint32_t x = state->a;
+	x ^= x << 13;
+	x ^= x >> 17;
+	x ^= x << 5;
+	return state->a = x;
+}
