418345fc71974424b87265ee269bbfeedf521738
Marshal Horn <kamocat@gmail.com>
2022-03-31 12:49:36 -0700

    Testing a a pipelined SPI to see if it's faster

--- code/main.c
+++ code/main.c
@@ -5,90 +5,64 @@
 #define F_CPU 1000000UL // 1MHz
 #include <util/delay.h>
 
-
-/* PWM for BLUE ESC goes from 1.1ms to 1.9ms, centered at 1.5ms */
-#define PWM_CENTER 384
-
-
-/* FUNCTIONS */
-
-/* This function does not require the ADC to be initialized beforehand.
- * It takes about 75us to run the first time, and 50uS every time after that.
- */
-uint8_t adc_sample( uint8_t channel ) {
-	channel = (channel & 0x1F) | 0x20; // sanitize the input
-	if( channel != ADMUX ) { 		// if it's not already the right channel
-		while( ADCSRA & 0x40 );	// wait for the current conversion to complete
-		ADMUX = channel;			// set the right channel
-	}
-	ADCSRA = 0xC0;			// start the conversion!
-	while( ADCSRA & 0x40 );	// wait for the conversion
-	return ADCH;
-}
-
-/* This is the same as adc_sample, but it returns a 10-bit result instead.
- * Because of the higher resolution, it runs a at a slower clock speed
- * It takes about 225uS the first time, and 130uS every time after that.
- */
-uint16_t adc_sample10( uint8_t channel ) {
-	uint16_t result = 0;
-	channel = (channel & 0x1F); // sanitize the input
-	if( channel != ADMUX ) { 		// if it's not already the right channel
-		while( ADCSRA & 0x40 );	// wait for the current conversion to complete
-		ADMUX = channel;			// set the right channel
-	}
-	ADCSRA = 0xC3;			// start the conversion!
-	while( ADCSRA & 0x40 );	// wait for the conversion
-	result = ADCL;
-	result |= ADCH<<8;
-	return result;
-}
-
-
 ISR( BADISR_vect ) {
 	//catch anything we accidentally enabled
 }
 
-#define DEBUG 1
+/* Code for the MAX7219 7seg driver */
+void spi_send(uint8_t * buf, uint8_t n){
+    const uint8_t hi = 0x11; // SPI master, set clock high
+    const uint8_t lo = 0x13; // SPI master, set clock low and shift data
+    ++n;
+    PORTB &= ~(1<<4); //Set PB4 low
+    while(--n){
+        USIDR = *buf++;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+        USICR = hi;
+        USICR = lo;
+    }
+    PORTB |= (1<<4); //Set PB4 hi
+}
+
+void spi_cmd(uint8_t cmd, uint8_t val){
+    uint8_t buf[2];
+    buf[0] = cmd;
+    buf[1] = val;
+    spi_send(buf, 2);
+}
+
+void init_7seg(void){
+    spi_cmd(9, 0xFF); // Control digits not individual segments
+    spi_cmd(10, 8);  // Intensity 8. (range is 0-15)
+    spi_cmd(11, 7);  // Display all 8 digits
+    spi_cmd(12, 1);  // Enable the display
+}
+
 int main( void ) {
 	
-	int16_t volts = 0;
-	uint16_t pwm = PWM_CENTER;
 	
 	/* PORT SETUP */
 	DDRA = 0x00;	// All of Port A is inputs
 	DIDR0 = 0xF0;	// disable digital input buffers on pA5 through pA7
 	DDRB = 0x7F;	// set all PORTB as outputs, except RESET.
 	
-	#if DEBUG == 2
-	/* PWM SETUP */
-	TCCR1A = 0x11; //PWM on OCR1B, with inverted and non-inverted output
-	TCCR1B = 0x01; 	// prescaler of 1
-	TCCR1D = 0x00; 	// Fast PWM
-	PLLCSR = 0x02; 	// Enable fast PLL (64 Mhz)
-	#elif DEBUG == 1
-	
-	/* PWM SETUP */
-	TCCR1A = 0x82; 	//PWM on OCR1A, with non-inverted output
-	TCCR1B = 0x03; 	// prescaler of 4
-	TCCR1D = 0x00; 	// Fast PWM
-	TC1H = 0x03;
-	OCR1C = 0xFF;		// Set TOP to max value
-	#endif
-	
+    init_7seg();
 	while(1) {
-
-		#if DEBUG == 1
-		volts = adc_sample(0);
-		pwm = PWM_CENTER + (128 - volts);
-		TC1H = pwm>>8;
-		OCR1A = pwm;
-		#else
-		/* Simple blinking light */
-		PINB = 0x08;
-		_delay_ms( 500 );
-		#endif
-		
+      spi_cmd(1, 0xA5);
+	  _delay_ms(1);
 	}
 
 	return 0;
