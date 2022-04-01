5d8cba2df2b73b9b4c3b67ba199e0d5d345a750c
Marshal Horn <kamocat@gmail.com>
2022-03-31 22:21:06 -0700

    Added variable-speed controls

--- code/main.c
+++ code/main.c
@@ -10,6 +10,16 @@ ISR( BADISR_vect ) {
 	//catch anything we accidentally enabled
 }
 
+uint16_t ms_elapsed(void){ 
+    return( TCNT0L | (TCNT0H << 8));
+}
+
+void ms_reset(void){
+    TCNT0H = 0;
+    TCNT0L = 0;
+    TCCR0A = 0x80;  // 16-bit counter
+    TCCR0B = 0x0D;  // Count milliseconds, clear prescaler
+}
 
 int main( void ) {
 	
@@ -21,22 +31,37 @@ int main( void ) {
     TCCR1B = 1;     // Start PWM clock at full speed
     TCCR1C = 0x09;  // Enable PWM on D
     TCCR1D = 0;     // Fast PWM
-	
+
     init_7seg();
     uint8_t line = 0;
     uint8_t enc = 0;
     int16_t num[2];
     num[0] = 100;
     num[1] = 200;
+    ms_reset();
 	while(1) {
+      uint16_t elapsed = ms_elapsed();
       enc <<= 4;
       enc |= PINA & 7; // Only look at pA0-2
-      if(enc == 0x45)
-          --num[line];
-      if(enc == 0x54)
-          ++num[line];
-      if(enc == 0x73)
+      if(enc == 0x45){
+          if(elapsed < 80)
+              num[line] -= 100;
+          if(elapsed < 150)
+              num[line] -= 10;
+          else
+              --num[line];
+          ms_reset();
+      } else if(enc == 0x54){
+          if(elapsed < 80)
+              num[line] += 100;
+          if( elapsed < 150)
+              num[line] += 10;
+          else
+              ++num[line];
+          ms_reset();
+      } else if(enc == 0x73){
           line ^= 1;
+      }
       write_line(num[0], 3, 0);
       write_line(num[1], 3, 1);
       TC1H = num[0]>>8;
