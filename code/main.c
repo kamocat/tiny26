305d6c5c404c1742b7ba60cec957d03f1eb35c91
Marshal Horn <kamocat@gmail.com>
2022-04-01 11:56:28 -0700

    Variable-prescaler PWM

--- code/main.c
+++ code/main.c
@@ -1,14 +1,10 @@
 #include <stdint.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include "7seg.h"
+#include "pwm.h"
 
 #define F_CPU 1000000UL // 1MHz
 #include <util/delay.h>
-
-ISR( BADISR_vect ) {
-	//catch anything we accidentally enabled
-}
 
 uint16_t ms_elapsed(void){ 
     return( TCNT0L | (TCNT0H << 8));
@@ -22,54 +18,50 @@ void ms_reset(void){
 }
 
 int main( void ) {
-	
-	/* PORT SETUP */
-	DDRA = 0x00;	// All of Port A is inputs
-    PORTA = 0x07;   // Enable pull-ups on pA0-2
-	DIDR0 = 0xF0;	// disable digital input buffers on pA5 through pA7
-	DDRB = 0x7F;	// set all PORTB as outputs, except RESET.
-    TCCR1B = 1;     // Start PWM clock at full speed
-    TCCR1C = 0x09;  // Enable PWM on D
-    TCCR1D = 0;     // Fast PWM
-
-    init_7seg();
+    /* Variable declaration */
     uint8_t line = 0;
     uint8_t enc = 0;
     int8_t speed = 0;
     struct mag pulse = {.hunds=1,.exponent=-4};
     struct mag period = {.hunds=1,.exponent=-3};
+
+    /* PORT SETUP */
+    DDRA = 0x00;	// All of Port A is inputs
+    PORTA = 0x07;   // Enable pull-ups on pA0-2
+    DIDR0 = 0xF0;	// disable digital input buffers on pA5 through pA7
+    DDRB = 0x7F;	// set all PORTB as outputs, except RESET.
+
+    /* Peripheral Init */
+    init_pwm();
+    init_7seg();
     ms_reset();
-	while(1) {
-      uint16_t elapsed = ms_elapsed();
-      if( elapsed < 80 )
-          speed = 3;
-      else if( elapsed < 150 )
-          speed = 2;
-      else
-          speed = 1;
 
-      enc <<= 4;
-      enc |= PINA & 7; // Only look at pA0-2
+    while(1) {
+        uint16_t elapsed = ms_elapsed();
+        if( elapsed < 80 )
+            speed = 3;
+        else if( elapsed < 150 )
+            speed = 2;
+        else
+            speed = 1;
 
-      if(enc == 0x45){
-          inc_mag(line?&period:&pulse, -speed);
-          ms_reset();
-      } else if(enc == 0x54){
-          inc_mag(line?&period:&pulse, speed);
-          ms_reset();
-      } else if(enc == 0x73){
-          line ^= 1;
-      }
-      write_mag(&period, 0);
-      write_mag(&pulse, 1);
+        enc <<= 4;
+        enc |= PINA & 7; // Only look at pA0-2
 
-      /* TODO: Resolve magnitude with prescaler
-      TC1H = pulse>>8;
-      OCR1D = pulse;
-      TC1H = period>>8;
-      OCR1C = period;
-      */
-	}
+        if(enc == 0x45){
+            inc_mag(line?&period:&pulse, -speed);
+            ms_reset();
+        } else if(enc == 0x54){
+            inc_mag(line?&period:&pulse, speed);
+            ms_reset();
+        } else if(enc == 0x73){
+            line ^= 1;
+        }
+        write_mag(&pulse, 0);
+        write_mag(&period, 1);
+        update_pwm(&pulse, &period);
 
-	return 0;
+    }
+
+    return 0;
 }
---
+++ code/pwm.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+void init_pwm(void);
+void update_pwm(const struct mag * pulse, const struct mag * period);
